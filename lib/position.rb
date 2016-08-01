df35d5bb9ace186d180a10646f6f26be787b66bd
Elliott Hilaire <elliott.hilaire@gmail.com>
2016-08-01 18:09:21 +0100

    Add validation to place

--- lib/position.rb
+++ lib/position.rb
@@ -3,6 +3,8 @@ class Position
   attr_reader :x, :y
 
   def initialize(x:, y:)
+    fail ArgumentError unless x.between? [0..4]
+    fail ArgumentError unless y.between? [0..4]
     @x = x
     @y = y
   end
