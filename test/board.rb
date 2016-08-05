3a630c0d7404c8ee10795731a83fec1cd9d5da57
Elliott Hilaire <elliott.hilaire@gmail.com>
2016-08-05 16:49:18 +0100

    Add Board

---
+++ test/board.rb
@@ -0,0 +1,38 @@
+require 'minitest/autorun'
+require_relative '../lib/board'
+
+describe Board do
+
+  describe "defaults" do
+    describe "#safe?" do
+      describe "0,0" do
+        it "is safe" do
+          board = Board.new
+          board.safe?(x:0, y:0).must_equal true
+        end
+      end
+
+      describe "4,4" do
+        it "is safe" do
+          board = Board.new
+          board.safe?(x:4, y:4).must_equal true
+        end
+      end
+
+      describe "5,5" do
+        it "is not safe" do
+          board = Board.new
+          board.safe?(x:5, y:5).must_equal false
+        end
+      end
+
+      describe "-1,-1" do
+        it "is not safe" do
+          board = Board.new
+          board.safe?(x:-1, y:-1).must_equal false
+        end
+      end
+
+    end
+  end
+end
