7e3fcd0d6e91996a5811f564aeb8a8f3b0f35289
Elliott Hilaire <elliott.hilaire@gmail.com>
2016-08-08 16:40:54 +0100

    Update board

--- test/board_test.rb
+++ test/board_test.rb
@@ -7,28 +7,28 @@ describe ToyRobot::Board do
     describe "#safe?" do
       describe "0,0" do
         it "is safe" do
-          board = Board.new
+          board = ToyRobot::Board.new
           board.safe?(x:0, y:0).must_equal true
         end
       end
 
       describe "4,4" do
         it "is safe" do
-          board = Board.new
+          board = ToyRobot::Board.new
           board.safe?(x:4, y:4).must_equal true
         end
       end
 
       describe "5,5" do
         it "is not safe" do
-          board = Board.new
+          board = ToyRobot::Board.new
           board.safe?(x:5, y:5).must_equal false
         end
       end
 
       describe "-1,-1" do
         it "is not safe" do
-          board = Board.new
+          board = ToyRobot::Board.new
           board.safe?(x:-1, y:-1).must_equal false
         end
       end
